cef7888a53fd8d66ac5c3b3e2e564c3cee90734d
Stijn Moreels <stijn.moreels@codit.eu>
2018-05-21 13:48:46 +0200

    Updates package with valid specs

--- src/System.Domain/Spec.cs
+++ src/System.Domain/Spec.cs
@@ -2,13 +2,30 @@
 
 namespace System.Domain
 {
+    /// <summary>
+    /// Representation of the model validation.
+    /// </summary>
     public static class Spec
     {
+        /// <summary>
+        /// Determines whether the specified x is true.
+        /// </summary>
+        /// <param name="x">if set to <c>true</c> [x].</param>
+        /// <returns>
+        ///   <c>true</c> if the specified x is true; otherwise, <c>false</c>.
+        /// </returns>
         public static bool IsTrue(bool x)
         {
             return x;
         }
 
+        /// <summary>
+        /// Determines whether the specified x is false.
+        /// </summary>
+        /// <param name="x">if set to <c>true</c> [x].</param>
+        /// <returns>
+        ///   <c>true</c> if the specified x is false; otherwise, <c>false</c>.
+        /// </returns>
         public static bool IsFalse(bool x)
         {
             return !x;
@@ -19,34 +36,83 @@ namespace System.Domain
             return x != null;
         }
 
+        /// <summary>
+        /// Determines whether the specified x matches a specifieid pattern.
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="pattern">The pattern.</param>
+        /// <returns></returns>
         public static bool Matches(this string x, string pattern)
         {
             return new Regex(pattern, RegexOptions.Compiled).IsMatch(x);
         }
 
+        /// <summary>
+        /// Determines whether the specified x is greater than the specified minimum and smaller than the specified maximum inclusive (min &lt;= x &lt;= max).
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="min">The minimum.</param>
+        /// <param name="max">The maximum.</param>
+        /// <returns></returns>
         public static bool InclusiveBetween(this int x, int min, int max)
         {
-            return min <= x && x >= max;
+            return min <= x && x <= max;
         }
 
+        /// <summary>
+        /// Determines whether the specified x is greater than the specified minimum and smaller than the specified maximum exclusive (min &lt; x &lt; max).
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="min">The minimum.</param>
+        /// <param name="max">The maximum.</param>
+        /// <returns></returns>
         public static bool ExclusiveBetween(this int x, int min, int max)
         {
-            return min < x && x > max;
+            return min < x && x < max;
         }
 
+        /// <summary>
+        /// Boolean AND operator.
+        /// </summary>
+        /// <param name="x">if set to <c>true</c> [x].</param>
+        /// <param name="y">if set to <c>true</c> [y].</param>
+        /// <returns></returns>
         public static bool And(this bool x, bool y)
         {
             return x && y;
         }
 
+        /// <summary>
+        /// Boolean AND operator with lazy right value.
+        /// </summary>
+        /// <param name="x">if set to <c>true</c> [x].</param>
+        /// <param name="y">The y.</param>
+        /// <returns></returns>
         public static bool And(this bool x, Func<bool> y)
         {
             return x && y();
         }
 
+        /// <summary>
+        /// Boolean OR operator.
+        /// </summary>
+        /// <param name="x">if set to <c>true</c> [x].</param>
+        /// <param name="y">if set to <c>true</c> [y].</param>
+        /// <returns></returns>
         public static bool Or(this bool x, bool y)
         {
             return x || y;
+        }
+
+        /// <summary>
+        /// Boolean OR operator with lazy right value.
+        /// </summary>
+        /// <param name="x">if set to <c>true</c> [x].</param>
+        /// <param name="y">The y.</param>
+        /// <returns></returns>
+        public static bool Or(this bool x, Func<bool> y)
+        {
+            return x || y();
         }
     }
 }
--- tests/System.Domain.Tests/ModelTests.cs
+++ tests/System.Domain.Tests/ModelTests.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Functional;
 using System.Functional.Monads;
+using System.IO;
 using System.Linq;
+using System.Xml.Serialization;
 using FsCheck.Xunit;
 using static System.Domain.Spec;
 
@@ -69,7 +72,7 @@ namespace System.Domain.Tests
         [Property]
         public bool Value_Gets_Only_Been_Read_Once(object x, FsCheck.PositiveInt times)
         {
-            IEnumerable<Maybe<object>> xs = 
+            IEnumerable<Maybe<object>> xs =
                 Enumerable.Repeat(ReadOnce.Wrap(x), times.Get)
                           .Select(f => f());
 
@@ -86,6 +89,19 @@ namespace System.Domain.Tests
             return name.Text.Matches("^[a-zA-Z\\. ]+$")
                        .And(age.Number.ExclusiveBetween(0, 100))
                        .Equals(Person.Maybe(name, age) != Maybe<Person>.Nothing);
+        }
+
+        [Property]
+        public bool Serialize_Person()
+        {
+            string xml =
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?><Person><Name>philip</Name><Age>53</Age></Person>";
+
+            var serializer = new XmlSerializer(typeof(PersonXml));
+            return new StringReader(xml)
+                    .Use(r => (PersonXml) serializer.Deserialize(r))
+                    .PipeTo(Person.FromXml)
+                != Maybe<Person>.Nothing;
         }
 
         public class Person
@@ -114,6 +130,32 @@ namespace System.Domain.Tests
                     .ThenMaybe(name.TupleWith(age))
                     .Select(t => new Person(t.Item1, t.Item2));
             }
+
+            public static Maybe<Person> FromXml(PersonXml p)
+            {
+                return p.Name.Unwrap(NonEmptyString.Maybe)
+                 .Zip(p.Age.Unwrap(PositiveInt.Maybe), Maybe)
+                 .Flatten();
+            }
+        }
+
+        [Serializable]
+        [DesignerCategory("code")]
+        [XmlType(AnonymousType = true)]
+        [XmlRoot(ElementName = "Person", Namespace = "", IsNullable = false)]
+        public class PersonXml
+        {
+            [XmlElement("Name")]
+            public string NameXml { get; set; }
+
+            [XmlIgnore]
+            public Untrusted<string> Name => Untrusted.Wrap(NameXml);
+
+            [XmlElement("Age")]
+            public int AgeXml { get; set; }
+
+            [XmlIgnore]
+            public Untrusted<int> Age => Untrusted.Wrap(AgeXml);
         }
     }
 }
